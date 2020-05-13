b0a284ac2722ed617d1192113dc2e475b0b18287
Stig Omdal <stigping@gmail.com>
2020-05-13 10:21:36 +0200

    Week 2 - 1+2
    Added week 2 article 1 changes
    Added week 2 article 2 changes

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,15 @@ function doAnswer3() {
     document.getElementById('answer1').innerHTML = '';
     document.getElementById('answer2').innerHTML = '';
 }
+function doSomething1() {
+    console.log('ondblclick was triggered!');
+}
+function doSomething2() {
+    console.log('oncontextmenu was triggered!');
+}
+function doSomething3() {
+    console.log('onkeydown was triggered!');
+}
+function doSomething4() {
+    console.log('onkeyup was triggered!');
+}
