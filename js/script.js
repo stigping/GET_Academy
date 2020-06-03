60f2689810ef9fa006800633b72a3fe504ce3a30
Stig Omdal <stigping@gmail.com>
2020-06-03 10:42:36 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,109 @@
-//M
-let valid = null;
-let dotValid = null;
-let yearValid = null;
-let fullDate = null;
-let lengthValid = null;
-let dotIndex1 = null;
-let dotIndex2 =null;
-let year = null;
-//V
+function isDateValid(dato) {
+    const year = dato.substr(6);
+    return checkLength(dato)
+        && checkPunctuation(dato)
+        && checkYear(dato)
 
-
-
-//C
-function isDateValid(date) {
-    fullDate = date;
-    year = date.substr(6);
-
-    checkLength()
-    checkPunctuation()
-
-    if (lengthValid == 'true' && dotValid == 'true') valid = 'true';
-    else valid = 'false';
-    return valid;
+    
 }
 
-function checkLength() {
-    if (fullDate.length == 10) return lengthValid = 'true';
-    else lengthValid = 'false';
+function checkLength(fullDato) {
+    return fullDato.length >= 10 
 }
 
-function checkPunctuation() {
-    if (fullDate.charAt(2) == '.' && fullDate.charAt(5) == '.') return dotValid = 'true';
-    else dotValid = 'false';
+function checkPunctuation(fullDato) {
+    return fullDato.charAt(2) == '.' && fullDato.charAt(5) == '.' 
 }
 
-function checkYear() {
-    if (year.length === 4 && year >= '0000' && year <= '9999') return yearValid = 'true';
-    else yearValid = 'false';
+function checkYear(year) {
+    return year.length === 4 && year >= '0000' && year <= '9999'
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var isValid = false;
+
+// function isThisAValidDate(riktigDato) {
+//     var dot1Position = riktigDato.indexOf(".");
+//     var dot2Posistion = riktigDato.substring(dot1Position + 1).indexOf(".");
+//     var checkIsOnlyNumbersAndDots = riktigDato.replace(/\./g, '');
+//     var checkLengthYearIsFour = riktigDato.substring(dot2Posistion + 4);
+//     var checkDayDateIsValid = parseInt(riktigDato.substring(0, 2));
+//     var checkMonthDateIsValid = parseInt(riktigDato.substring(dot2Posistion + 1, dot2Posistion + 3));
+
+//     isValid = dateLength(riktigDato);
+
+//     isValid = checkdot(dot1Position, dot2Posistion);
+
+//     isValid = onlyDigitsValid(checkIsOnlyNumbersAndDots);
+
+//     isValid = fourCharOnly(checkLengthYearIsFour);
+
+//     isValid = dateValid(checkDayDateIsValid, checkMonthDateIsValid, checkLengthYearIsFour);
+
+//     isValid = monthIsValid(checkMonthDateIsValid);
+
+//     return isValid;
+// }
+
+// function dateLength(riktigDato) {
+//     return riktigDato.length == 10 ? true : false;
+// }
+
+// function checkdot(dot1Position, dot2Posistion) {
+//     return isValid == true && dot1Position == 2 && dot2Posistion == 2 ? true : false;
+// }
+
+// function onlyDigitsValid(checkIsOnlyNumbersAndDots) {
+//     return isValid == true && checkIsOnlyNumbersAndDots.match(/^[0-9]+$/) ? true : false;
+// }
+
+// function fourCharOnly(checkLengthYearIsFour) {
+//     return isValid == true && checkLengthYearIsFour.length == 4 && checkLengthYearIsFour >= 0000 ? true : false;
+// }
+
+// function monthIsValid(checkMonthDateIsValid) {
+//     return isValid == true && checkMonthDateIsValid >= 0 && checkMonthDateIsValid <= 12 ? true : false;
+// }
+
+// function max31DaysInMonth(monthValid) {
+//     if (monthValid == 1 || monthValid == 3 || monthValid == 5 || monthValid == 7 || monthValid == 8 || monthValid == 10 || monthValid == 12) {
+//         return true;
+//     } else return false;
+// }
+
+// function max30DaysInMonth(monthValid) {
+//     if (monthValid == 4 || monthValid == 6 || monthValid == 9 || monthValid == 11) {
+//         return true;
+//     } else return false;
+// }
+
+// function isItMonth2(checkDayDateIsValid, checkMonthDateIsValid) {
+//     if (checkDayDateIsValid >= 1 && checkDayDateIsValid <= 28 && checkMonthDateIsValid == 2) return true;
+// }
+
+// function isItLeapYear(checkDayDateIsValid, checkMonthDateIsValid, checkLengthYearIsFour) {
+//     var isItLeapYear = ((checkLengthYearIsFour % 4 == 0) && (checkLengthYearIsFour % 100 != 0)) || (checkLengthYearIsFour % 400 == 0);
+//     if (checkDayDateIsValid == 29 && checkMonthDateIsValid == 2 && isItLeapYear == true) {
+//         return true;
+//     } else return false;
+// }
+
+// function dateValid(checkDayDateIsValid, checkMonthDateIsValid, checkLengthYearIsFour) {
+//     if (isValid == true && checkDayDateIsValid >= 1 && checkDayDateIsValid <= 31 && max31DaysInMonth(checkMonthDateIsValid) ||
+//         isValid == true && checkDayDateIsValid >= 1 && checkDayDateIsValid <= 30 && max30DaysInMonth(checkMonthDateIsValid) ||
+//         isValid == true && isItMonth2(checkDayDateIsValid, checkMonthDateIsValid) ||
+//         isValid == true && isItLeapYear(checkDayDateIsValid, checkMonthDateIsValid, checkLengthYearIsFour)) {
+//         return true;
+//     } else return false;
+// }
--- js/test.js
+++ js/test.js
@@ -1,4 +1,9 @@
 QUnit.test( "Valid length", function( assert ) {
-    const isValid = isDateValid('17.01.2020');
-    assert.equal(isValid, 'true');
-  });
+  const isValid = isDateValid('03.02.2020');
+  assert.equal(isValid, true);
+});
+
+QUnit.test("Det er ikke punktum i begge de rette posisjonene", function (assert) {
+  const isValid = isDateValid('0310212020');
+  assert.equal(isValid, false);
+});
