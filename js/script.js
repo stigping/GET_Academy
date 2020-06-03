719ad30417496e77b7c3c85c70f920e6e5fc291b
Stig Omdal <stigping@gmail.com>
2020-06-03 22:48:16 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -1,38 +1,43 @@
 function isDateValid(dato) {
-    const year = dato.substring(6);
-    const month = dato.substring(3, 5);
-    const day = dato.substring(0, 2);
-
-    return checkLength(dato)
+   return  checkLength(dato)
         && checkPunctuation(dato)
-        && checkYear(year)
-        && checkMonth(month)
-        && isLeapYear(year)
-        && checkDay(day)
+        && checkYear(dato)
+        && checkMonth(dato)
+        && checkDay(dato)
     
 }
 
-function checkLength(fullDato) {
-    return fullDato.length >= 10;
+function checkLength(dato) {
+    return dato.length === 10;
 }
 
-function checkPunctuation(fullDato) {
-    return fullDato.charAt(2) == '.' && fullDato.charAt(5) == '.';
+function checkPunctuation(dato) {
+    return dato.charAt(2) == '.' && dato.charAt(5) == '.';
 }
 
-function checkYear(year) {
-    return year.length === 4 && year >= '0000' && year <= '9999';
+function checkYear(dato) {
+    const year = dato.substring(6);
+    return year.length === 4 && year >= 0000 && year <= 9999;
 } 
 
-function checkMonth(month) {
+function checkMonth(dato) {
+    const month = dato.substring(3, 5);
     return month.length === 2 && month >= '01' && month <= '12';
 } 
 
-function isLeapYear(year){
-    return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+function checkDay(dato) {
+    const day = dato.substring(0, 2);
+    const month = dato.substring(3, 5);
+    const year = dato.substring(6);
+    if (day.length === 2 && day >= '01' && day <= '28') return true;
+    if (day > '31') return false;
+    if (month != '02'){ 
+        return day <= '30' || month !== '04' && month  !== '06' && month  !== '09' && month  !== '11';
+    }
+    else {
+       return day == '29' && isLeapYear(year);
+    }
 }
-
-function checkDay(day) {
-    day.length === 2 && day >= '01' && day <= '28';
-    if (day == '29' && isLeapYear() == true) return
+function isLeapYear(year){
+    return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
 }
