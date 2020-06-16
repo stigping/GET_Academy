21721a57617ae6aec659acccf5f1fcba383cafb8
Stig Omdal <stigping@gmail.com>
2020-06-16 14:48:40 +0200

    Changes

---
+++ pages/js/controller.js
@@ -0,0 +1,15 @@
+//function translate(text) {
+function makeText() {
+    console.log('I funksjon: ', document.getElementById('inputs'));
+    show();
+ }
+/*function makeText(tekst) {
+    console.log('I funksjon: ', document.getElementById('inputs').value);
+    for (let i = 0; i < tekst.length; i++) {
+    inputText = tekst[i];
+    return translate(inputText);
+ }
+}
+
+//console.log('Sjekker funksjonen translate= ',translate('b'),translate(' '),translate('a'));
+//if(JSON.stringify(morseCodeJSON)==JSON.stringify(annenArray)) */
---
+++ pages/js/view.js
@@ -0,0 +1,24 @@
+var contentDiv = document.getElementById('content');
+var resultDiv = document.getElementById('result');
+let input = document.getElementById('inputs');
+
+//View
+function show() {
+    contentDiv.innerHTML = `
+    <input id='inputs'>
+    <input id='myBtn' type="button" onclick="makeText(input.value)" value="Clickme">
+    <div id="answer">${answer}</div>
+    `;
+    //answer.innerText += translate(makeText);
+
+    
+}
+
+
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13)
+    {
+        event.preventDefault()
+        document.getElementById("myBtn").click();
+    }
+});
